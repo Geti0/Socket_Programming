eb23602d8cca48f125a5ba55f7d04775fb833619
getuarkelmendi6 <getuarkelmendi6@gmail.com>
2023-12-01 00:15:33 +0100

    server and client

---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+import threading
+import wikipedia
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5566
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
