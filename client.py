786f3aae64fa8e3a9461e5b2bda3e0067698c27c
getuarkelmendi6 <getuarkelmendi6@gmail.com>
2023-12-02 15:19:23 +0100

    implemented request number 5 in server side

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
+
+
+
 import socket
 
 IP = socket.gethostbyname(socket.gethostname())
-PORT = 5566
+PORT = 1235
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
@@ -14,16 +17,24 @@ def main():
     print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
     connected = True
 
+    # Receive access granted message
+    access_msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER] {access_msg}")
+
     while connected:
-        msg = input("Type for something you want to search> ")
+        msg = input("Type for something you want to search or enter '!GET_FILE filename' to request a file> ")
 
         client.send(msg.encode(FORMAT))
 
         if msg == DISCONNECT_MSG:
             connected = False
+        elif msg.startswith("!GET_FILE "):
+            file_contents = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER] File contents:\n{file_contents}")
         else:
             msg = client.recv(SIZE).decode(FORMAT)
             print(f"[SERVER] {msg}")
+
 
 def file_type(type):
     match type:
@@ -40,7 +51,8 @@ def file_type(type):
         case 'execute':
             print("Execute..")
 
+
 if __name__ == "__main__":
     main()
-    type = input("Choose read, write or execute: ")
+    type = input("Choose read, write or execute: ").lower()
     print(file_type(type))
--- server.py
+++ server.py
@@ -1,29 +1,45 @@
 import socket
 import threading
-import wikipedia
+import os
 
 IP = socket.gethostbyname(socket.gethostname())
-PORT = 5566
+PORT = 1235
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
+ACCESS_GRANTED_MSG = "!ACCESS_GRANTED"
+DIRECTORY_PATH = "C:/Users/Getuar/Documents/GitHub/Socket_Programming"  # Change this to the desired directory path
+
+def send_file_contents(conn, filename):
+    file_path = os.path.join(DIRECTORY_PATH, filename)
+    try:
+        with open(file_path, 'r') as file:
+            contents = file.read()
+            conn.send(contents.encode(FORMAT))
+    except FileNotFoundError:
+        conn.send("File not found.".encode(FORMAT))
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     connected = True
+
+    # Send access granted message
+    conn.send(ACCESS_GRANTED_MSG.encode(FORMAT))
+
     while connected:
         msg = conn.recv(SIZE).decode(FORMAT)
         if msg == DISCONNECT_MSG:
             connected = False
-        print(f"[{addr}] {msg}")
-        # msg = f"Msg received: {msg}"
-        msg = wikipedia.summary(msg, sentences=1)
+        else:
+            print(f"[{addr}] {msg}")
 
-        conn.send(msg.encode(FORMAT))
+            # Check if the message is a request for file contents
+            if msg.startswith("!GET_FILE "):
+                filename = msg[len("!GET_FILE "):]
+                send_file_contents(conn, filename)
 
     conn.close()
-
 
 def main():
     print("[STARTING] Server is starting...")
@@ -37,9 +53,7 @@ def main():
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
 
-        print(f"[ACTIVE CONNECTIONS] "
-              f"{threading.active_count()-1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
 
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
